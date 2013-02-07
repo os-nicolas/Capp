6ff1794c2f5dbd7afdcca3bc903c15865462497a
colin <colin@ubuntu.ubuntu-domain>
2013-02-06 23:51:04 -0500

    work for 2-6

--- Capp/src/net/connivance/capp/MainActivity.java
+++ Capp/src/net/connivance/capp/MainActivity.java
@@ -1,18 +1,38 @@
 package net.connivance.capp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	ListView cappList;
+	Button left,right;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cappList = (ListView) findViewById(R.id.cap_holder);
+        
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+        		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+        		  "Linux", "OS/2" };
+
+        		// Define a new Adapter
+        		// First parameter - Context
+        		// Second parameter - Layout for the row
+        		// Third parameter - ID of the TextView to which the data is written
+        		// Forth - the Array of data
+
+        		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        		// Assign adapter to ListView
+        		cappList.setAdapter(adapter); 
     }
 
     @Override
